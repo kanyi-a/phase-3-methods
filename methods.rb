4d061c0c4bbebc51fbd3bd9de1ada970e3a41772
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-08-11 21:16:37 +0300

    commit tree 980af1fcde318e98a4f1181126c98205e210481b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+# function greetProgrammer() {
+#     console.log("Hello, programmer!");
+#   }
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# function greet(name) {
+#     console.log(`Hello, ${name}!`);
+#   }
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# function greetWithDefault(name = "programmer") {
+#     console.log(`Hello, ${name}!`);
+#   }
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# function add(num1, num2) {
+#     return num1 + num2;
+#   }
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+  num / 2
+end
